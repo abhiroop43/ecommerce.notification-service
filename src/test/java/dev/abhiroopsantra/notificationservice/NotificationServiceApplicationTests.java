6221b89e2189d14117ef6ce988ba0bb00d5574e4
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2024-02-11 12:12:16 +0400

    initial commit

---
+++ src/test/java/dev/abhiroopsantra/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.abhiroopsantra.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class NotificationServiceApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
